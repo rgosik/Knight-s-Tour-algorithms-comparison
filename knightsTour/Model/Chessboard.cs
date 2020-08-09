604536121601f53776252f07570875ebbb04bc8d
Rafal <grafal96@gmail.com>
2020-08-09 11:16:28 +0200

    Chessboard update

--- knightsTour/Model/Chessboard.cs
+++ knightsTour/Model/Chessboard.cs
@@ -10,39 +10,14 @@ namespace knightsTour.Model
     public class Chessboard
     {
         public int[,] Board { get; private set; }
-        public int KnightX { get; private set; }
-        public int KnightY { get; private set; }
         public int XSize { get; private set; }
         public int YSize { get; private set; }
-
-        public Chessboard()
-        {
-        }
 
         public Chessboard(int xSize, int ySize)
         {
             Board = new int[ySize, xSize];
             YSize = ySize;
             XSize = xSize;
-        }
-
-        public Chessboard(int xSize, int ySize, int knightX, int knightY) : this(xSize, ySize)
-        {
-            KnightX = knightX;
-            KnightY = knightY;
-        }
-
-        public Chessboard(int[,] board, int knightX, int knightY) : this(board.GetLength(1), board.GetLength(0))
-        {
-            KnightX = knightX;
-            KnightY = knightY;
-        }
-
-        public void MoveKnight(Move move, int iteration)
-        {
-            KnightX += move.X;
-            KnightY += move.Y;
-            Board[KnightY, KnightX] = iteration;
         }
 
         public Chessboard DeepCopy()
